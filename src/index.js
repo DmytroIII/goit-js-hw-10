dda26f6ab60d32ba0706587865354a34aeaa522c
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-10-22 15:29:27 +0300

    dz

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
 
 
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries.js';
+import markupList from './templates/countryList.hbs';
+import markupItem from './templates/countryItem.hbs';
 import './css/styles.css';
 
-/* 
-const DEBOUNCE_DELAY = 300; */
+const DEBOUNCE_DELAY = 300;
 
-//9153ea9818454af8838171652221410
+const inputSearch = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-// http://api.weatherapi.com/
-// http://api.weatherapi.com/v1/forecast.json?key=9153ea9818454af8838171652221410&q=Kharkiv&days=7
 
-const base_url = 'http://api.weatherapi.com/v1'
-const KEY = '9153ea9818454af8838171652221410'
-const City = 'Kharkiv'
-
-const container = document.querySelector('.list');
-
-const resp = fetch(`${base_url}/forecast.json?key=${KEY}&q=${City}&days=7`);
-
-resp.then(response => { 
-    if (!response.ok) { 
-        throw new Error()
+inputSearch.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+Notify.init({
+    position: "center-top"
+})
+function onSearchInput(e) {
+    clear()
+    const name = e.target.value.trim();
+    if (name === '') {
+        return Notify.info(
+            `It can't be empty field! Please, fill it up with at least two letters!`
+        );
     }
-    return response.json();
-}).then(data => { 
-    const markup = createMarkup(data.forecast.forecastday)
-    container.insertAdjacentHTML ('beforeend', markup)
-}).catch(err => console.log(err))
-
-function createMarkup(arr) { 
-    return arr.map(item => `<li>
-  <h2> Дата: ${item.date}  </h2>
-  <img src="${item.day.condition.icon}" alt="${item.day.condition.text}">
-    <p>${item.day.condition.text} </p>
-    <h3> Средняя температура: ${item.day.avgtemp_c}  </h3>
-    </li>`).join('')
+    fetchCountries(name)
+        .then(data => {
+            if (data.length > 10) {
+                return Notify.info(
+                    'Too many matches found. Please enter a more specific name.'
+                );
+            }
+            if (data.length === 1) {
+                countryInfo.insertAdjacentHTML('beforeend', markupItem(data))
+            }
+            if (data.length >= 2 && data.length <=10) {
+                return countryList.insertAdjacentHTML('beforeend', markupList(data))
+            }
+        })
+        .catch(err => Notify.failure("Oops, there is no country with that name"));
 }
-    
+
+function clear() {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+}
